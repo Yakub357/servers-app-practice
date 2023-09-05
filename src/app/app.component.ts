2241ffe0be010b602595fd0701afbb4bce277d32
Coding Raccoon <egamyakub357@gmail.com>
2023-09-05 21:50:20 +0800

    Basic structure

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,4 +5,30 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {}
+export class AppComponent {
+  serverElements = [];
+  serverName = '';
+  serverContent = '';
+
+  onAddServer() {
+    this.serverElements.push({
+      name: this.serverName,
+      content: this.serverContent,
+      type: 'server',
+    });
+
+    this.serverName = '';
+    this.serverContent = '';
+  }
+
+  onAddBlueprint() {
+    this.serverElements.push({
+      name: this.serverName,
+      content: this.serverContent,
+      type: 'blueprint',
+    });
+
+    this.serverName = '';
+    this.serverContent = '';
+  }
+}
